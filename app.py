a8def7e01ac2893d6b2b96c452fc02af7f0dabfb
unknowntrader7 <unknownkys@gmail.com>
2023-03-31 02:28:24 +0900

    5

--- app.py
+++ app.py
@@ -1,39 +1,28 @@
-from flask import Flask, render_template, request, jsonify, send_from_directory
-from dotenv import load_dotenv
+import sys
 import os
-from modules.papago_api import translate_text
-from modules.chatgpt_api import get_chatgpt_response
-from modules.polly_api import text_to_speech
+from flask import Flask, render_template, request
+from modules.papago import translate
+from modules.chatGPT import generate_answer
+from modules.polly import synthesize_speech
 
-load_dotenv()
+app = Flask(__name__)
 
-app = Flask(__name__, static_folder='static')
+# 모듈 경로 추가
+sys.path.append(os.path.abspath('./modules'))
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/translate', methods=['POST'])
-def translate():
-    text = request.json['text']
-    translated_text = translate_text(text)
-    return jsonify(translated_text=translated_text)
-
-@app.route('/chat', methods=['POST'])
-def chat():
-    text = request.json['text']
-    source_lang = 'ko'
-    target_lang = 'en'
-    translated_text = translate_text(text, source_lang, target_lang)
-    chatgpt_response = get_chatgpt_response(translated_text)
-    translated_chatgpt_response = translate_text(chatgpt_response, target_lang, source_lang)
-    return jsonify(chatgpt_response=translated_chatgpt_response)
-
-@app.route('/synthesize', methods=['POST'])
-def synthesize():
-    text = request.json['text']
-    audio_url = text_to_speech(text)
-    return jsonify(audio_url=audio_url)
+@app.route('/get_answer', methods=['POST'])
+def get_answer():
+    question = request.form['question']
+    lang = request.form['lang']
+    translated_question = translate(question, lang)
+    answer = generate_answer(translated_question)
+    translated_answer = translate(answer, 'ko')
+    speech = synthesize_speech(answer)
+    return {'question': question, 'answer': answer, 'translated_question': translated_question, 'translated_answer': translated_answer, 'speech': speech}
 
 if __name__ == '__main__':
     app.run(debug=True)

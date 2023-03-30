1dbedf9a2af0bda72eb49b76666e73af4d4e7d31
unknowntrader7 <unknownkys@gmail.com>
2023-03-31 01:49:58 +0900

    4

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request, jsonify, send_from_directory
 from dotenv import load_dotenv
 import os
-import modules.papago_api as papago_api
-import modules.chatgpt_api as chatgpt_api
-import modules.polly_api as polly_api
+from modules.papago_api import translate_text
+from modules.chatgpt_api import get_chatgpt_response
+from modules.polly_api import text_to_speech
 
 load_dotenv()
 
@@ -11,24 +11,28 @@ app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def index():
-    return rende_template('index.html')
+    return render_template('index.html')
 
 @app.route('/translate', methods=['POST'])
 def translate():
     text = request.json['text']
-    translated_text = papago_api.translate(text)
+    translated_text = translate_text(text)
     return jsonify(translated_text=translated_text)
 
 @app.route('/chat', methods=['POST'])
 def chat():
     text = request.json['text']
-    chatgpt_response = chatgpt_api.get_chatgpt_response(text)
-    return jsonify(chatgpt_response=chatgpt_response)
+    source_lang = 'ko'
+    target_lang = 'en'
+    translated_text = translate_text(text, source_lang, target_lang)
+    chatgpt_response = get_chatgpt_response(translated_text)
+    translated_chatgpt_response = translate_text(chatgpt_response, target_lang, source_lang)
+    return jsonify(chatgpt_response=translated_chatgpt_response)
 
 @app.route('/synthesize', methods=['POST'])
 def synthesize():
     text = request.json['text']
-    audio_url = polly_api.synthesize_speech(text)
+    audio_url = text_to_speech(text)
     return jsonify(audio_url=audio_url)
 
 if __name__ == '__main__':
--- modules/chatgpt_api.py
+++ modules/chatgpt_api.py
@@ -4,18 +4,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-async def get_chatgpt_response(prompt):
-    api_key = os.getenv('OPENAI_API_KEY')
-    openai.api_key = api_key
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
+def get_chatgpt_response(prompt):
+    model_engine = "text-davinci-002"
     response = openai.Completion.create(
-        engine="davinci",
+        engine=model_engine,
         prompt=prompt,
-        max_tokens=150,
+        max_tokens=1024,
         n=1,
         stop=None,
-        temperature=0.7,
+        temperature=0.5,
     )
-
-    chatgpt_response = response.choices[0].text.strip()
-    return chatgpt_response
+    return response.choices[0].text.strip()

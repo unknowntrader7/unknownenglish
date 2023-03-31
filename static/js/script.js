952a66829f8788c5bf019c4539794ea12748abe7
unknowntrader7 <unknownkys@gmail.com>
2023-03-31 16:22:54 +0900

    ver2

--- static/js/script.js
+++ static/js/script.js
@@ -1,44 +1,39 @@
-const inputText = document.getElementById('input-text');
-const sendButton = document.getElementById('send-button');
-const chatContainer = document.getElementById('chat-container');
+document.addEventListener("DOMContentLoaded", function () {
+  const gptForm = document.getElementById("gpt-form");
+  const messageList = document.getElementById("message-list");
 
-sendButton.addEventListener('click', sendMessage);
-inputText.addEventListener('keypress', (e) => {
-  if (e.key === 'Enter') {
-    sendMessage();
+  gptForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const userInput = document.getElementById("user-input");
+    const userMessage = userInput.value;
+    userInput.value = "";
+
+    addMessage("user", userMessage);
+
+    addMessage("bot", "답변 생성 중...");
+
+    const response = await fetch("/", {
+      method: "POST",
+      body: new FormData(e.target),
+    });
+    const data = await response.json();
+    const botMessage = data.response;
+
+    updateLastBotMessage(botMessage);
+  });
+
+  function addMessage(sender, text) {
+    const messageItem = document.createElement("li");
+    messageItem.classList.add("message", sender);
+    messageItem.innerText = text;
+    messageList.appendChild(messageItem);
+    messageList.scrollTop = messageList.scrollHeight;
+  }
+
+  function updateLastBotMessage(text) {
+    const lastBotMessage = messageList.querySelector(".bot:last-child");
+    lastBotMessage.innerText = text;
+    messageList.scrollTop = messageList.scrollHeight;
   }
 });
-
-function sendMessage() {
-  const message = inputText.value.trim();
-  if (!message) return;
-
-  // 사용자의 질문을 화면에 표시
-  displayMessage('user', message);
-  inputText.value = '';
-
-  // 서버에 질문 전송
-  fetch('http://127.0.0.1:5000/chat', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ message })
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      // 서버에서 받은 답변을 화면에 표시
-      displayMessage('bot', data.chatgpt_response);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-}
-
-function displayMessage(sender, message) {
-  const div = document.createElement('div');
-  div.className = sender === 'user' ? 'user-message' : 'bot-message';
-  div.textContent = message;
-  chatContainer.appendChild(div);
-  chatContainer.scrollTop = chatContainer.scrollHeight;
-}
